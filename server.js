ab4ce2805c8d26aa443fc54e4cb5634cbef0c64e
techinnovies <techinnovies@gmail.com>
2021-03-12 20:43:59 +0530

    test

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/sniffer-angular'));
 
-// app.get('/*', function(req,res) {
-//   res.sendFile(path.join(__dirname+'/dist/sniffer-angular/index.html'));
-// });
-
-app.get('*', (req, res) => {
-  res.sendFile(`./dist/sniffer-angular/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/sniffer-angular/index.html'));
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
